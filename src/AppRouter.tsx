ab43299fd6105001f456960f8817a4554fa73304
hzrd149 <github@hzrd149.com>
2026-01-19 13:02:24 -0600

    Add relay management settings from marmot chat app
    - Import relay management components from marmot-ts chat app
    - Add RelayListInput component for managing relay lists
    - Add RelayItem component with connection status and removal
    - Add NewRelayForm component for adding new relays
    - Add RelayAvatar component for displaying relay icons
    - Create Settings page with 4 relay sections: Lookup, Outbox, Inbox, Extra
    - Add /settings route to AppRouter
    - Temporarily disable /examples route (missing components)

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -2,7 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ScrollToTop } from "./components/ScrollToTop";
 
 import Index from "./pages/Index";
-import Examples from "./pages/Examples";
+// import Examples from "./pages/Examples";
+import Settings from "./pages/Settings";
 import { NIP19Page } from "./pages/NIP19Page";
 import NotFound from "./pages/NotFound";
 
@@ -12,7 +13,8 @@ export function AppRouter() {
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path="/examples" element={<Examples />} />
+        {/* <Route path="/examples" element={<Examples />} /> */}
+        <Route path="/settings" element={<Settings />} />
         {/* NIP-19 route for npub1, note1, naddr1, nevent1, nprofile1 */}
         <Route path="/:nip19" element={<NIP19Page />} />
         {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
